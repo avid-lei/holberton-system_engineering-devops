f684e8e4e2b7175927d353c6451ed897ba5e7b07
lei-diva <divaaleii@gmail.com>
2020-02-04 07:05:40 +0000

    3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""export dict list of dict"""
+
+if __name__ == '__main__':
+    import json
+    import requests
+
+    urlID = requests.get('https://jsonplaceholder.typicode.com/users/')
+    jID = urlID.json()
+
+    users = {}
+    for user in jID:
+        tasks = []
+        user_id = user.get('id')
+        username = user.get('username')
+        td = requests.get('https://jsonplaceholder.typicode.com/todos' +
+                          '?userId={:}'.format(user_id))
+        jtd = td.json()
+
+        for x in jtd:
+            user = {}
+            user['username'] = username
+            user['task'] = x.get('title')
+            user['completed'] = x.get('completed')
+            tasks.append(user)
+
+        users[user_id] = tasks
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(users, f)
