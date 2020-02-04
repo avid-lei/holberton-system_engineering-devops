413a38e1861587b6bec8afc9fe05021c46c218f4
lei-diva <divaaleii@gmail.com>
2020-02-04 06:38:05 +0000

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""export in CSV format"""
+
+if __name__ == '__main__':
+    import json
+    import requests
+    from sys import argv
+
+    urlID = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                         format(argv[1]))
+
+    jID = urlID.json()
+    name = jID.get('username')
+
+    urlTD = requests.get('https://jsonplaceholder.typicode.com/todos')
+    jTD = urlTD.json()
+
+    tasks = []
+
+    for x in jTD:
+        user = {}
+        if str(x.get("userId")) == argv[1]:
+                user['username'] = name
+                user['task'] = str(x.get('title'))
+                user['completed'] = str(x.get('completed'))
+                tasks.append(user)
+
+    users = {argv[1]: tasks}
+    with open(argv[1] + ".json", "w") as f:
+        f.write(json.dumps(users))
