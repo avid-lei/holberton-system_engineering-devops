54624f5d9383e21c783e114fec669b42d8a7a24d
lei-diva <divaaleii@gmail.com>
2020-02-11 12:34:38 +0000

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,15 +9,15 @@ def top_ten(subreddit):
                ' Intel Mac OS X 10_10_1) AppleWebKit/537.36 ' +
                '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    http = requests.get('https://www.reddit.com/r/{:}/hot.json?limit=10'
-                        .format(subreddit), headers=headers,
-                        allow_redirects=False)
+    http = requests.get('https://www.reddit.com/r/{}/top.json'
+                        .format(subreddit), params={'limit': '10'},
+                        headers=headers, allow_redirects=False)
 
     if http.status_code == 404:
         print(None)
     else:
         http = http.json()
         data = http.get('data')
-        lists = data.get('children')
-        for l in lists:
+        data = data.get('children')
+        for l in data:
             print(l.get('data').get('title'))
