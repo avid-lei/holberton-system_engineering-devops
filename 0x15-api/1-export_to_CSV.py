b01f0e794c202e77b1845434bc9ac4b170ddb2b0
lei-diva <divaaleii@gmail.com>
2020-02-11 10:41:28 +0000

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""export in CSV format"""
 
 if __name__ == '__main__':
     import csv
     import requests
-    from sys import argv
+    import sys
 
-    urlID = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                         format(argv[1]))
+    user_id = sys.argv[1]
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(user_id))
+    username = user.json().get('username')
 
-    jID = urlID.json()
-    name = jID.get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                         params={'userId': user_id})
+    todos = todos.json()
 
-    urlTD = requests.get('https://jsonplaceholder.typicode.com/todos')
-    jTD = urlTD.json()
-
-    with open(argv[1] + ".csv", 'w', newline='') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC,)
-
-        for x in jTD:
-            if str(x.get("userId")) == argv[1]:
-                com = str(x.get('completed'))
-                title = str(x.get('title'))
-                writer.writerow([argv[1], name, com, title])
+    with open('{}.csv'.format(user_id), 'w') as f:
+        writer = csv.writer(f)
+        for task in todos:
+            writer.writerow([user_id, username, task.get('completed'),
+                            task.get('title')])
