35c75daea69753caffbbc945f73a4253a8b3b641
lei-diva <divaaleii@gmail.com>
2020-02-11 12:35:16 +0000

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
                ' Intel Mac OS X 10_10_1) AppleWebKit/537.36 ' +
                '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    http = requests.get('https://www.reddit.com/r/{}/top.json'
+    http = requests.get('https://www.reddit.com/r/{:}/top.json'
                         .format(subreddit), params={'limit': '10'},
                         headers=headers, allow_redirects=False)
 
