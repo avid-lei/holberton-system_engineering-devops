1f5ca2fb0348672132b3d5f142ea1d4ad3e25aa4
lei-diva <divaaleii@gmail.com>
2020-02-11 12:23:31 +0000

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Reddit API for top 10 of subreddit"""
+import requests
 
 
 def top_ten(subreddit):
-    import requests
+
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh;' +
                ' Intel Mac OS X 10_10_1) AppleWebKit/537.36 ' +
                '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -12,7 +13,7 @@ def top_ten(subreddit):
                         .format(subreddit), params={'limit': 10},
                         headers=headers, allow_redirects=False)
 
-    if http.status_code == 404:
+    if http.status_code >= 300:
         print('None')
     else:
         http = http.json()
